e77cab0ffd11680ed598cb704eed6aa2bd4cc8e1
lasiu01 <114987939+lasiu01@users.noreply.github.com>
2024-04-17 11:05:30 +0200

    Revert "Merge branch 'master' of https://github.com/PwSGiVR-2024/Diablol"
    This reverts commit bdc544e466ef18d426160bea5cd77a5f8ab01133, reversing
    changes made to 2afea0765ab74c22601c0bb10dd6e8252cbe7b22.

--- Assets/skrypty/point and click.cs
+++ Assets/skrypty/point and click.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class pointandclick : MonoBehaviour
 {
-    private Vector3 destination;
-    private int speed = 5;
+//private 
     // Start is called before the first frame update
     void Start()
     {
-        destination = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
-        if (Input.GetMouseButtonDown(0)) Move(); 
-    }
-
-    public void Move()
-    {
-        RaycastHit hit;
+        
     }
 }
