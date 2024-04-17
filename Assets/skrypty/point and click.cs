b40d3e121bcaf26e8544db699ee001ee7756c6d4
Czaplaii <czapla.jakub2@gmail.com>
2024-04-17 10:55:30 +0200

    test merge

--- Assets/skrypty/point and click.cs
+++ Assets/skrypty/point and click.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class pointandclick : MonoBehaviour
 {
-//private 
+    private Vector3 destination;
+    private int speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        destination = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+        if (Input.GetMouseButtonDown(0)) Move(); 
+    }
+
+    public void Move()
+    {
+        RaycastHit hit;
     }
 }
