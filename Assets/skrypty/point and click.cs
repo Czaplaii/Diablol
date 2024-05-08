deaeff79ee5b3123be82787c1d088a96775f4fc5
SebiXonomnom <28878@s.pm.szczecin.pl>
2024-05-08 10:37:10 +0200

    Sterowanie oraz ikona kursora
    Dodanie sterowania P&C oraz ikonki kursora

--- Assets/skrypty/point and click.cs
+++ Assets/skrypty/point and click.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class pointandclick : MonoBehaviour
 {
-//private 
-    // Start is called before the first frame update
+    private Vector3 targetPosition;
+    private int speed = 5;
     void Start()
     {
-        
+        targetPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+
+        if (Input.GetMouseButtonDown(0)) Move();
+    }
+
+    void Move()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            targetPosition = hit.point + new Vector3(0, 0.5f, 0);
+        }
     }
 }
