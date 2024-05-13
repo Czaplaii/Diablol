fb3e8cc8413ad6f393f773d5819fb7b347ccfb7b
lasiu01 <28871@s.pm.szczecin.pl>
2024-05-13 19:30:33 +0200

    Efekt kliknięcia przy poruszaniu się

--- Assets/skrypty/point and click.cs
+++ Assets/skrypty/point and click.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class pointandclick : MonoBehaviour
 {
+    public GameObject clickEffectPrefab;
+    public float effectLifetime = 3.0f;
+
     private Vector3 targetPosition;
     private int speed = 5;
+
     void Start()
     {
         targetPosition = transform.position;
@@ -16,16 +19,36 @@ public class pointandclick : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
 
-        if (Input.GetMouseButtonDown(0)) Move();
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Move(out hit);
+            PlayClickEffect(hit);
+        }
     }
 
-    void Move()
+    void Move(out RaycastHit hit)
     {
-        RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit))
         {
             targetPosition = hit.point + new Vector3(0, 0.5f, 0);
+        }
+    }
+
+    void PlayClickEffect(RaycastHit hit)
+    {
+        if (clickEffectPrefab != null)
+        {
+            Vector3 clickPosition = hit.point;
+            clickPosition.z = 0;
+            GameObject effect = Instantiate(clickEffectPrefab, clickPosition, Quaternion.identity);
+            Rigidbody rb = effect.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.up * 5f;
+            }
+            Destroy(effect, effectLifetime);
         }
     }
 }
